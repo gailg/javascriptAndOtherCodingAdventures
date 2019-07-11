963552ebe91dc08701c76e85290c07b6c201f159
Gail Gong <gailgongster@gmail.com>
2019-07-11 16:26:17 -0700

    I've gone through Matrix and MatrixIterator, and maybe I get a little of it

--- 2019-july/2019-07-08-put-this-in-git/eloquent-javascript/06-a-rabbit.js
+++ 2019-july/2019-07-08-put-this-in-git/eloquent-javascript/06-a-rabbit.js
@@ -93,3 +93,81 @@ console.log(anos);
 console.log(`Julia is ${anos.get("Julia")} years old.`);
 console.log(`Is Jack's age known?  ${anos.has("Jack")}`);
 console.log(`Is toString's age known?  ${anos.has("toString")}`);
+
+console.log("----------------------------------------------------------symbols");
+let symbol = Symbol("name");
+console.log('-----symbol == Symbol("name")');
+console.log(symbol == Symbol("name"));
+Rabbit.prototype[symbol] = 55;
+console.log("montyRabbit[symbol]");
+console.log(montyRabbit[symbol]);
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function() {
+  return `${this.length} cm of blue yarn`;
+}
+console.log("-----[1, 2].toString()");
+console.log([1, 2].toString());
+console.log("-----[1, 2][toStringSymbol]()");
+console.log([1, 2][toStringSymbol]());
+let stringObject = {
+  [toStringSymbol]() {
+    return "a jute rope"}
+}
+console.log("-----stringObject[toStringSymbol]()");
+console.log(stringObject[toStringSymbol]());
+
+console.log("-----------------------------------------------iterator interface");
+let roseDragonIterator = "🌹🐉"[Symbol.iterator]();
+console.log("-----roseDragonIterator.next()");
+console.log(roseDragonIterator.next());
+console.log("-----roseDragonIterator.next()");
+console.log(roseDragonIterator.next());
+console.log("-----roseDragonIterator.next()");
+console.log(roseDragonIterator.next());
+class Matrix {
+  constructor(width, height, element = (x, y) => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+  next() {
+    if (this.y == this.matrix.height) return {done: true};
+    let value = {x: this.x,
+                 y: this.y,
+                 value: this.matrix.get(this.x, this.y)};
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return {value, done: false};
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+  return new MatrixIterator(this);
+}
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x},${y}`);
+for (let {x, y, value} of matrix) {
+  console.log(x, y, value);
+}
