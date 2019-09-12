db90b0d3522ff3dc7715c49ae16fda6a40fa3be0
Gail Gong <gailgongster@gmail.com>
2019-09-11 17:00:34 -0700

    both kevin-powell/31-portfolio and wes-boss image gallery

--- 2019-july/2019-07-08-put-this-in-git/scrimba-javascript/01-arrow-functions.js
+++ 2019-july/2019-07-08-put-this-in-git/scrimba-javascript/01-arrow-functions.js
@@ -5,7 +5,7 @@ console.log(`------------------------------------------------------arrow functio
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = (arr) => {
   const squaredIntegers = arr
-    .filter(number => Number.isInteger(number) && number > 0)
+    .filter(number => Number.isInteger(number) && number > 0);
     .map(positive => positive * positive);
   return squaredIntegers;
 };
---
+++ 2019-july/2019-07-08-put-this-in-git/wes-bos-css-grid/css-grid-image-gallery/script.js
@@ -0,0 +1,34 @@
+const gallery = document.querySelector(".gallery");
+const overlay = document.querySelector(".overlay");
+const overlayImage = overlay.querySelector("img");
+const overlayClose = overlay.querySelector("close");
+
+function generateHTML([h, v]) {
+    return `
+        <div class="item h${h} v${v}">
+        <img src="images/acrylic-pouring-${randomNumber(10)}.jpg">
+        <div class="item__overlay">
+        <button>View</button>
+        </div>
+        </div>
+    `;
+}
+function randomNumber(upperLimit) {
+    return Math.floor(Math.random() * upperLimit + 1);
+}
+const digits = Array.from({ length: 50}, () =>
+    [randomNumber(4), randomNumber(4)]).concat([
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1],
+        [1,1], [1,1], [1,1], [1,1], [1,1], [1,1], [1,1]
+    ])
+const html = digits.map(generateHTML).join("");
+gallery.innerHTML = html;
